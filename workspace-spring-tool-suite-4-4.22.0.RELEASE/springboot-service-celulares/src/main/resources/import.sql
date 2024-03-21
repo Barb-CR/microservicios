49bc25d56a0596feb768e6e98a86f64c8783273b
Barb-CR <barbarbaracantu@gmail.com>
2024-03-20 22:19:39 -0600

    video1_oficial

---
+++ workspace-spring-tool-suite-4-4.22.0.RELEASE/springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO celulares(name, marca, created_at) VALUES('Samsung', 'S21', NOW());
+INSERT INTO celulares(name, marca, created_at) VALUES('Samsung', 'S22', NOW());
+INSERT INTO celulares(name, marca, created_at) VALUES('Motorola', 'G20', NOW());
+INSERT INTO celulares(name, marca, created_at) VALUES('iPHONE', '11MAX', NOW());
+INSERT INTO celulares(name, marca, created_at) VALUES('iPHONE', '10', NOW());
